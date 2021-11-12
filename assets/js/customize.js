00b231adb2576fe496a54604366a6c9fdcc1a398
sbwww <nj_bwshen@outlook.com>
2021-11-12 18:42:33 +0800

    replace anchor jump to scroll, update content

---
+++ assets/js/customize.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".header").click(function () {
+    $.scrollTo("#header", 500);
+  });
+  $(".milestones").click(function () {
+    $.scrollTo("#milestones", 500);
+  });
+  $(".articles").click(function () {
+    $.scrollTo("#articles", 500);
+  });
+});
